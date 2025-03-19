650f0b8325723bfca63dc5a82b0b8a1da5731b31
savvasSid <minisid12@hotmail.com>
2025-03-19 16:22:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ function submitForm() {
     document.getElementById('contactForm').style.display = 'none';
     document.getElementById('successMessage').style.display = 'block';
   }
+
+
+
+
+
+
+
+
+
+
+
